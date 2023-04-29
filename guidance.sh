b65b764a87ee0dc9f67c87e3272190a0ab6283e8
Bugra Oezdemir <bugra.oezdemir@embl.de>
2023-04-29 08:36:58 +0000

    Update guidance.sh

--- guidance.sh
+++ guidance.sh
@@ -1,3 +1,3 @@
 git clone -b dev https://git.embl.de/oezdemir/OME_Zarr_Tools.git &&
-source OME_Zarr_Tools/install.sh
+source OME_Zarr_Tools/install.sh T0XMlxMdq8C6rSxurrdqMqHNrhyhC4f0 dRFXoR852egFtp3lC9NJPYjpPaCBNRa8
 
