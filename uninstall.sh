3c87fdb3e100daa8e0e8082772a05663bb2b4122
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-05-03 17:15:21 +0000

    Update and rename uninstall.sh to del_all.sh

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-unalias fiji
-rm -rf ~/Applications;
-rm -rf ~/OME_Zarr_Tools
-rm -rf ~/miniconda3
-
-echo "export PATH=/opt/munge-0.5.12/bin:/opt/slurm-21.08.8/bin:/opt/slurm-21.08.8/sbin:/opt/nhc-1.4.2/sbin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin" > ~/.bashrc
-source  ~/.bashrc
